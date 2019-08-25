d4eee7df14809ab1222adb9bc04013db59ba688f
oscarkremer <oscar.s.kremer@hotmail.com>
2019-08-25 02:06:57 -0300

    add: Estimator train

--- src/api/model_parameters.py
+++ src/api/model_parameters.py
@@ -1,13 +1,12 @@
 import os
 import numpy as np 
 import matplotlib.pyplot as plt 
-
+from src.models import Estimator
 
 if __name__=='__main__':
+    y = []
     t = np.arange(0, 0.5001, 0.001)
     u = [np.sin(t),np.cos(t), 3*np.power(t, 2)]
-    y, theta_plot = [], []
-
     for i in range(t.shape[0]):
         if t[i] < 0.3:
             y.append([3*u[0][i] + 2*u[1][i] + 6*u[2][i]])
@@ -15,19 +14,8 @@ if __name__=='__main__':
             y.append([2*u[0][i] + 2*u[1][i] + 2*u[2][i]])
            
     y = np.array(y)
-    P = 100000*np.identity(3)
-    p = np.zeros(t.shape[0])
-    theta = [0, 0, 0]
-    for i in range(t.shape[0]):
-        theta_plot.append(theta)
-        p[i] = np.linalg.norm(P, ord='fro')
-        fi = np.array([[u[0][i]], [u[1][i]], [u[2][i]]])
-        K = P.dot(fi)/(1 + (np.transpose(fi).dot(P)).dot(fi))
-        P = (np.identity(3) - K.dot(np.transpose(fi))).dot(P)
-        if t[i] in [0.1, 0.2, 0.3, 0.4]:
-            P = 100000*np.identity(3)
-        theta = theta + K.dot(y[i] - np.transpose(fi).dot(theta))
-
+    model = Estimator(0.1, 0.5)
+    theta_plot, p = model.train(t, u, y)
     plt.plot(t,p)
     plt.show()
 
--- src/models/model.py
+++ src/models/model.py
@@ -3,44 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 class Estimator:
-    def __init__(self, delta_time):
-        self.time = delta_time
-
-    def train(self,)
-        t = np.arange(0, 0.5001, 0.001)
-        u1 = np.sin(t)
-        u2 = np.cos(t)
-        u3 = 3*np.power(t, 2)
-        y = []
-        for i in range(t.shape[0]):
-            if t[i] < 0.3:
-                y.append([3*u1[i] + 2*u2[i] + 6*u3[i]])
-            else:
-                y.append([2*u1[i] + 2*u2[i] + 2*u3[i]])
-            
-        y = np.array(y)
+    def __init__(self, delta_time, final_time):
+        self.update_time = delta_time
+        self.final_time = final_time
+ 
+    def train(self, t, u, y):
+        theta_plot = []
         P = 100000*np.identity(3)
         p = np.zeros(t.shape[0])
         theta = [0, 0, 0]
-        theta_plot = []
+        time_list = self.create_timelist()
         for i in range(t.shape[0]):
             theta_plot.append(theta)
             p[i] = np.linalg.norm(P, ord='fro')
-            fi = np.array([[u1[i]], [u2[i]], [u3[i]]])
+            fi = np.array([[u[0][i]], [u[1][i]], [u[2][i]]])
             K = P.dot(fi)/(1 + (np.transpose(fi).dot(P)).dot(fi))
             P = (np.identity(3) - K.dot(np.transpose(fi))).dot(P)
-            if t[i] in [0.1, 0.2, 0.3, 0.4]:
+            if t[i] in time_list:
                 P = 100000*np.identity(3)
             theta = theta + K.dot(y[i] - np.transpose(fi).dot(theta))
+        return theta_plot, p
+           
+    def create_timelist(self):
+        time_list = []
+        append_time = self.update_time
+        while append_time < self.final_time:
+            time_list.append(np.round(append_time,2))
+            append_time+=self.update_time
+        return time_list
 
-        plt.plot(t,p)
-        plt.show()
-
-        theta1 = np.transpose(np.array(theta_plot))[0]
-        theta2 = np.transpose(np.array(theta_plot))[1]
-        theta3 = np.transpose(np.array(theta_plot))[2]
-        
-        plt.plot(t, theta1)
-        plt.plot(t, theta2)
-        plt.plot(t, theta3)
-        plt.show()
