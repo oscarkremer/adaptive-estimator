df5e233c370ab8bc1e43d7a2332fb902aaf0535a
oscarkremer <oscar.s.kremer@hotmail.com>
2019-08-26 23:47:39 -0300

    add-new-adaptive-estimators

--- src/api/multiple.py
+++ src/api/multiple.py
@@ -3,33 +3,55 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from src.models import Estimator
 
+def f(t):
+    return np.random.uniform(low=0, high=4, size=t.shape[0])
+
 if __name__=='__main__':
-    y ,models, thetas, p = [], [], [], []
+    u, y ,models, thetas, p = [], [], [], [], []
     step = 0.001
     final_time = 0.500
     t = np.arange(0, step + final_time, step)
-    update_times = [0.01, 0.03, 0.05, 0.07]
+    update_times = [0.51]
+#    update_times = [0.03, 0.05, 0.07]
     #update_times = [0.01, 0.05, 0.08, 0.1, 0.15, 0.2, 0.3]
-    u = [np.sin(t), np.cos(t), 3*np.power(t, 2)]
-    weights = [[3, 2, 6],[10, 12, 6],[20, 0, 30]]
-    print(weights[0][0])
-    print(weights[1][0])
-    print(weights[2][0])
-    
+    #u = np.array([np.sin(t), np.cos(t), 3*np.power(t, 2)])
+    #print(u.shape)    
+#    weights = np.array([3*np.ones(t.shape[0]) + 40*np.sin(10*t), 2*np.ones(t.shape[0]) + 1.1*np.sin(2*t), 6*np.ones(t.shape[0])])
+#    y = np.sum(weights*u,axis=0)
+#    print(y.shape)
+    sig = f(t)
+
     for i in range(t.shape[0]):
-        if np.round(t[i],3) < 0.25:
-            y.append([weights[0][0]*u[0][i] + weights[0][1]*u[1][i] + weights[0][2]*u[2][i]])
+        if i == 0:
+            y.append(0)
+            u.append([0, 0, 0, 0])
         else:
-            if np.round(t[i],3) < 0.42:
-                y.append([weights[1][0]*u[0][i] + weights[1][1]*u[1][i] + weights[1][2]*u[2][i]])
+            if i == 1:
+                y.append(0.7*sig[i-1])
+                u.append([y[i-1], 0, sig[i-1], 0])
             else:
-                y.append([weights[2][0]*u[0][i] + weights[2][1]*u[1][i] + weights[2][2]*u[2][i]])  
+                if i == 2:
+                    y.append(-0.7*y[i-1] - 0.1*y[i-2] + 1*sig[i-1] - 0.3*sig[i-2])        
+                    u.append([y[i-1], y[i-2], sig[i-1], sig[i-2]])
+                else:
+                    y.append(-0.7*y[i-1] - 0.1*y[i-2] + 1*sig[i-1] - 0.3*sig[i-2])
+                    u.append([y[i-1], y[i-2], sig[i-1], sig[i-2]])
+    u = np.transpose(np.array(u))
+    print(u.shape)
+#    print(size(y))
+#    for i in range(t.shape[0]):
+#        if np.round(t[i],3) < 0.25:
+#            y.append([weights[0][0]*u[0][i] + weights[0][1]*u[1][i] + weights[0][2]*u[2][i]])
+#        else:
+#            if np.round(t[i],3) < 0.26:
+#                y.append([weights[1][0]*u[0][i] + weights[1][1]*u[1][i] + weights[1][2]*u[2][i]])
+#            else:
+#                y.append([weights[2][0]*u[0][i] + weights[2][1]*u[1][i] + weights[2][2]*u[2][i]])  
     analysis = 'error'
 #    analysis = 'mean'
     y = np.array(y)
     for update_time in update_times:
         model = Estimator(update_time, step + final_time)
-        print(model)
         model.train(t, u, y)
         models.append(model)
 
@@ -42,9 +64,7 @@ if __name__=='__main__':
         else:
             for model in models:
                 error_list.append(model.mean[i])
-                print(model.mean[i])
             index = np.argmin(np.array(error_list))
-        print(index)
    #     print(models[index])
 
         thetas.append(models[index].theta_plot[i])
@@ -65,15 +85,12 @@ if __name__=='__main__':
     plt.plot(t,p)
     plt.show()
 
-    plt.plot(t, y)
-    plt.plot(t, y_hat)
-    plt.show()
-
-
-#    plt.plot(t, np.transpose(np.array(thetas))[0])
-#    plt.plot(t, np.transpose(np.array(thetas))[1])
-#    plt.plot(t, np.transpose(np.array(thetas))[2])
-#    plt.show()
+    for model in models:
+        plt.plot(t, np.transpose(np.array(model.theta_plot))[0])
+        plt.plot(t, np.transpose(np.array(model.theta_plot))[1])
+        plt.plot(t, np.transpose(np.array(model.theta_plot))[2])
+        plt.plot(t, np.transpose(np.array(model.theta_plot))[3])
+        plt.show()
 
     plt.plot(t, error)
     plt.show()
--- src/models/model.py
+++ src/models/model.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 class Estimator:
-    def __init__(self, delta_time, final_time, rastreabilty = 100000):
+    def __init__(self, delta_time, final_time, rastreabilty = 1000000):
         self.rastreabilty = rastreabilty
         self.update_time = delta_time
         self.final_time = final_time
@@ -16,28 +16,27 @@ class Estimator:
             append_time+=self.update_time
         return time_list
 
-    def train(self, t, u, y):
+    def train(self, t, u, y, lambda_forg=1):
         error, theta_plot = [], []
-        self.P = self.rastreabilty*np.identity(3)
+        self.P = self.rastreabilty*np.identity(4)
         p = np.zeros(t.shape[0])
         y_hat = np.zeros(t.shape[0])
         norm = np.zeros(t.shape[0])
         mean = np.zeros(t.shape[0])
-        theta = [0, 0, 0]
+        theta = [0, 0, 0, 0]
         time_list = self.create_timelist()
         for i in range(t.shape[0]):
             if np.round(t[i], 3) in time_list:
-                self.P = self.rastreabilty*np.identity(3)
-            error.append(abs(y[i] - theta[0]*u[0][i] - theta[1]*u[1][i] - theta[2]*u[2][i])/y[i])
+                self.P = self.rastreabilty*np.identity(4)
+            error.append(abs(y[i] - theta[0]*u[0][i] - theta[1]*u[1][i] - theta[2]*u[2][i] - theta[3]*u[3][i])/y[i])
             p[i] = np.linalg.norm(self.P, ord='fro')
-            norm[i] = p[i]/np.linalg.norm(self.rastreabilty*np.identity(3), ord='fro')
-            mean[i] = 1/(0.5*(norm[i] + (1/error[i])))
+            norm[i] = p[i]/np.linalg.norm(self.rastreabilty*np.identity(4), ord='fro')
             theta_plot.append(theta)
-            fi = np.array([[u[0][i]], [u[1][i]], [u[2][i]]])
-            K = self.P.dot(fi)/(1 + (np.transpose(fi).dot(self.P)).dot(fi))
-            self.P = (np.identity(3) - K.dot(np.transpose(fi))).dot(self.P)
+            fi = np.array([[u[0][i]], [u[1][i]], [u[2][i]], [u[3][i]]])
+            K = self.P.dot(fi)/(lambda_forg + (np.transpose(fi).dot(self.P)).dot(fi))
+            self.P = (np.identity(4) - K.dot(np.transpose(fi))).dot(self.P)/lambda_forg
             theta = theta + K.dot(y[i] - np.transpose(fi).dot(theta))
-            y_hat[i] = theta_plot[i][0]*u[0][i] + theta_plot[i][1]*u[1][i] + theta_plot[i][2]*u[2][i]
+            y_hat[i] = theta_plot[i][0]*u[0][i] + theta_plot[i][1]*u[1][i] + theta_plot[i][2]*u[2][i] + theta_plot[i][3]*u[3][i]
         self.y_hat = y_hat
         self.error = error
         self.theta_plot= theta_plot
